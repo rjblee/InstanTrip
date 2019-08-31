ee5ccb69b80e6a4be56e458f66fadc2ea72194df
Jiadan <jiadanwang@gmail.com>
2019-08-31 00:18:27 +0000

    Add map

---
+++ final-client/src/components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+
+
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDtGZmEeW3QEK20irH8SpIpdKQjPoKuW5U'
+})(MapContainer);
