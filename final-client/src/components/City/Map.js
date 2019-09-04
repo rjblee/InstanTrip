0065383f1ad07cea58287d9305d92b160e498eab
Jiadan <jiadanwang@gmail.com>
2019-09-04 22:33:22 +0000

    add route and info box

--- final-client/src/components/City/Map.js
+++ final-client/src/components/City/Map.js
@@ -20,7 +20,9 @@ export default function Map() {
         {lat: 49.246292, lng: -123.116226},
         {lat: 49.267132, lng: -122.968941},
         {lat: 49.166592, lng: -123.133568},
-      ]
+        {lat:49.2384, lng:-123.0318},
+        {lat:49.2483,lng:-123.0559}
+        ]
       //code from here
       //maker
       markerPositions.map(loc => {
@@ -43,7 +45,17 @@ export default function Map() {
         {
           location: {lat: 49.166592, lng: -123.133568},
           stopover: true
-      }
+      },
+      {
+        location: {lat:49.2384, lng:-123.0318},
+        stopover: true
+    },
+      {
+        location: {lat:49.2483,lng:-123.0559},
+        stopover: true
+  }
+      
+        
       ];
       var directionsDisplay = new google.maps.DirectionsRenderer;
       var directionsService = new google.maps.DirectionsService;
@@ -60,21 +72,31 @@ export default function Map() {
           directionsService.route({
             origin: markerPositions[0], 
             destination: markerPositions[1], 
-            waypoints: waypoints, //an array of waypoints
+            // waypoints: waypoints, //an array of waypoints
             // optimizeWaypoints: true,
-            travelMode: "DRIVING"
+            travelMode: "TRANSIT",
+            transitOptions: {
+              departureTime: new Date(Date.now() + 100000),
+              modes: ['BUS'],
+              routingPreference: 'FEWER_TRANSFERS'
+            }
+            
           }, function(response, status) {
+            console.log('here is the response ----------')
+            console.log(response)
             if (status == 'OK') {
               directionsDisplay.setDirections(response);
-              console.log(response)
+              console.log("-----response",response)
+              
+              var infowindow2 = new google.maps.InfoWindow();
+              infowindow2.setContent(response.routes[0].legs[0].distance.text + "<br>" + response.routes[0].legs[0].duration.text + " ");
+              infowindow2.setPosition({lat: 49.166592, lng: -123.133568});
+              infowindow2.open(targetMap);
             } else {
               window.alert('Directions request failed due to ' + status);
             }
           });
         }
-
-
-
 
 
     })
