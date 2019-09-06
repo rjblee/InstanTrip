de1872ea012b8407671ebeb8b7a657e019198357
Jiadan <jiadanwang@gmail.com>
2019-09-06 23:59:42 +0000

    add steps

--- final-client/src/components/City/City.js
+++ final-client/src/components/City/City.js
@@ -3,16 +3,17 @@ import Wishlist from "./Wishlist";
 import "../../styles/City.css";
 import Map from './Map';
 import SearchBar from '../SearchBar/searchBar'
-import createAndSaveSchecules from '../../helpers/createAndSaveSchecules'
+import createAndSaveSchecules from '../../helpers/createAndSaveSchecules';
+import Demo from './Step'
 
 export default function City(props) {
 
   const [foundPlaces, setfoundPlaces] = useState([])
 
 
-  console.log('city-----------')
-  console.log(props.city)
-  console.log(props.places)
+  // console.log('city-----------')
+  // console.log(foundPlaces)
+  // console.log(props.places)
 
 
   console.log('result from kMeanClustering')
@@ -34,24 +35,36 @@ export default function City(props) {
   // createAndSaveSchecules(props.places, 2)
 
 
+  // let lng;
+  // let lat;
 
+  // if (foundPlaces.length > 0) {
+  //   lng = foundPlaces[0].lng
+  //   lat = foundPlaces[0].lat
+  // }
 
   return(
     <>
       <p> here is the city page</p>
       <SearchBar setplaces={setfoundPlaces} city={props.city}/>
-
-      <div class="container">
+      
+      <div>
            <div class="row">
-            <div class="col-5">
+           <div class="col-1" ></div>
+            <div class="col-6">
             <Map></Map>
             </div>
-            <div class="col-2" ></div>
-            <div class="col-5">
+            <div class="col-4">
             <Wishlist></Wishlist>
             </div>
+            <div class="col-1" ></div>
           </div>
-        </div>
+      </div>
+      <div><Demo /></div>
+   
+       
+          
+        
     </>
 
 
