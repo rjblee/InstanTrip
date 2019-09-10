04507b1d4bab295a4308eb55ca626291798aa821
Ralph <ralph.lee@live.ca>
2019-09-10 00:25:07 +0000

    Add alert notification for successfully adding wishlist item

--- final-client/src/components/City/AllPlaceItem.js
+++ final-client/src/components/City/AllPlaceItem.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import "../../styles/City.css";
-import addScheduleIdToPlace from "../../helpers/addScheduleIdToPlace"
+import addScheduleIdToPlace from "../../helpers/addScheduleIdToPlace";
+import "react-toastify/dist/ReactToastify.css";
+import AlertButton from './Alert';
 
 export default function AllPlaceItem(props) {
   const [targetScheduleId, setTargetScheduleId] = useState('Schedule ID')
@@ -24,30 +26,27 @@ export default function AllPlaceItem(props) {
           return <option value={schedule.id}>{schedule.id}</option>
         })}
       </select>
-      <button
-
-        className="add-button"
-
+      <AlertButton   
         onClick={() => {
           addScheduleIdToPlace(props.place.id, targetScheduleId, props.setUser)
           setTargetScheduleId('Schedule ID')
         }}
-
-      ><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjBweCIgeT0iMHB4Igp3aWR0aD0iNjAiIGhlaWdodD0iNjAiCnZpZXdCb3g9IjAgMCAzMCAzMCIKc3R5bGU9IiBmaWxsOiMwMDAwMDA7Ij4gICAgPHBhdGggZD0iTTE1LDNDOC4zNzMsMywzLDguMzczLDMsMTVjMCw2LjYyNyw1LjM3MywxMiwxMiwxMnMxMi01LjM3MywxMi0xMkMyNyw4LjM3MywyMS42MjcsMywxNSwzeiBNMjAsMTZoLTR2NCBjMCwwLjU1My0wLjQ0OCwxLTEsMXMtMS0wLjQ0Ny0xLTF2LTRoLTRjLTAuNTUyLDAtMS0wLjQ0Ny0xLTFzMC40NDgtMSwxLTFoNHYtNGMwLTAuNTUzLDAuNDQ4LTEsMS0xczEsMC40NDcsMSwxdjRoNCBjMC41NTIsMCwxLDAuNDQ3LDEsMVMyMC41NTIsMTYsMjAsMTZ6Ij48L3BhdGg+PC9zdmc+"/></button>
-
-   
+        text={"👍 " + props.place.name + " has been added"}
+      >
+       
+      </AlertButton>
     
-  
+
   
 
       
-      <button
+      {/* <button
         // className="example_f"
         onClick={() => {
           // addScheduleIdToPlace(props.place.id, targetScheduleId, props.setUser)
           // setTargetScheduleId('Schedule ID')
         }}
-      >Delete</button>
+      >Delete</button> */}
     </div>
 
   )
